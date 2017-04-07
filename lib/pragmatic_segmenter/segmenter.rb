40821e9098bcf1e7a594e7e3d5567cabed9c0678
BigRon <jdraths@alumni.nd.edu>
2017-04-07 14:05:34 -0500

    Update segmenter.rb

--- lib/pragmatic_segmenter/segmenter.rb
+++ lib/pragmatic_segmenter/segmenter.rb
@@ -9,10 +9,11 @@ module PragmaticSegmenter
     def initialize(text:, language: 'en', doc_type: nil, clean: true)
       return unless text
       @language = language
-      @language_module = Languages.get_language_by_code(language)
+      @language_module = Languages.get_language_by_code(language) # this results in a language (i.e. PragmaticSegmenter::Languages::English)
       @doc_type = doc_type
 
       if clean
+        # cleaner (below) is defined as a class; .clean (for English) runs clean_quotations
         @text = cleaner.new(text: text, doc_type: @doc_type, language: @language_module).clean
       else
         @text = text
